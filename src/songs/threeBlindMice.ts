d8df319fa7f7aecc1b4be5d7f9b358ed728a5815
gkskehls <gkskels@gmail.com>
2025-07-15 22:08:42 +0900

    feat: Add lyrics to 'Three Blind Mice'

--- src/songs/threeBlindMice.ts
+++ src/songs/threeBlindMice.ts
@@ -1,14 +1,16 @@
 import { SongNote } from './types';
 
 export const threeBlindMice: SongNote[][] = [
+  // 1소절: Three blind mice, Three blind mice
   [
-    { note: 'E4', duration: 'q' }, { note: 'D4', duration: 'q' },
-    { note: 'C4', duration: 'h' }, { note: 'E4', duration: 'q' },
-    { note: 'D4', duration: 'q' }, { note: 'C4', duration: 'h' },
+    { note: 'E4', duration: 'q', lyric: 'Three' }, { note: 'D4', duration: 'q', lyric: 'blind' },
+    { note: 'C4', duration: 'h', lyric: 'mice' }, { note: 'E4', duration: 'q', lyric: 'Three' },
+    { note: 'D4', duration: 'q', lyric: 'blind' }, { note: 'C4', duration: 'h', lyric: 'mice' },
   ],
+  // 2소절: See how they run, See how they run
   [
-    { note: 'G4', duration: 'q' }, { note: 'F4', duration: 'q' },
-    { note: 'E4', duration: 'h' }, { note: 'G4', duration: 'q' },
-    { note: 'F4', duration: 'q' }, { note: 'E4', duration: 'h' },
+    { note: 'G4', duration: 'q', lyric: 'See' }, { note: 'F4', duration: 'q', lyric: 'how' },
+    { note: 'E4', duration: 'h', lyric: 'they run' }, { note: 'G4', duration: 'q', lyric: 'See' },
+    { note: 'F4', duration: 'q', lyric: 'how' }, { note: 'E4', duration: 'h', lyric: 'they run' },
   ],
 ];
