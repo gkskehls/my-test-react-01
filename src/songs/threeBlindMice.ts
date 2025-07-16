941f85796aae6eb6d80df0b8c25e24fb48d55525
p.sysy1@kepartner.com <p.sysy1@kepartner.com>
2025-07-16 11:29:42 +0900

    기능: 'Three Blind Mice' 악보 전체 추가
    기존에 일부만 구현되어 있던 'Three Blind Mice' 악보를 전체 곡으로 완성하여 사용자가 끝까지 연습할 수 있도록 했습니다.

--- src/songs/threeBlindMice.ts
+++ src/songs/threeBlindMice.ts
@@ -13,4 +13,29 @@ export const threeBlindMice: SongNote[][] = [
     { note: 'E4', duration: 'h', lyric: 'they run' }, { note: 'G4', duration: 'q', lyric: 'See' },
     { note: 'F4', duration: 'q', lyric: 'how' }, { note: 'E4', duration: 'h', lyric: 'they run' },
   ],
+  // 3소절: They all ran after the farmer's wife
+  [
+    { note: 'G4', duration: 'q', lyric: 'They' }, { note: 'A4', duration: 'q', lyric: 'all' },
+    { note: 'G4', duration: 'q', lyric: 'ran' }, { note: 'F4', duration: 'q', lyric: 'af-' },
+    { note: 'E4', duration: 'q', lyric: 'ter' }, { note: 'D4', duration: 'q', lyric: 'the' },
+    { note: 'C4', duration: 'h', lyric: 'wife,' },
+  ],
+  // 4소절: Who cut off their tails with a carving knife
+  [
+    { note: 'G4', duration: 'q', lyric: 'Who' }, { note: 'A4', duration: 'q', lyric: 'cut' },
+    { note: 'G4', duration: 'q', lyric: 'off' }, { note: 'F4', duration: 'q', lyric: 'their' },
+    { note: 'E4', duration: 'q', lyric: 'tails' }, { note: 'D4', duration: 'q', lyric: 'with a' },
+    { note: 'C4', duration: 'h', lyric: 'knife,' },
+  ],
+  // 5소절: Did you ever see such a sight in your life
+  [
+    { note: 'C4', duration: 'q', lyric: 'Did' }, { note: 'D4', duration: 'q', lyric: 'you' },
+    { note: 'E4', duration: 'h', lyric: 'e-ver' }, { note: 'C4', duration: 'q', lyric: 'see' },
+    { note: 'D4', duration: 'q', lyric: 'such a' }, { note: 'E4', duration: 'h', lyric: 'sight,' },
+  ],
+  // 6소절: As three blind mice
+  [
+    { note: 'D4', duration: 'q', lyric: 'As' }, { note: 'D4', duration: 'q', lyric: 'three' },
+    { note: 'C4', duration: 'h', lyric: 'blind mice.' },
+  ],
 ];
