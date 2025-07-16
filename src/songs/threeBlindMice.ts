2ca80e2768bc5a5704eaf954b4893740e079960d
p.sysy1@kepartner.com <p.sysy1@kepartner.com>
2025-07-16 11:43:46 +0900

    수정: threeBlindMice 악보의 가사 시스템 마이그레이션 및 타입 오류 해결
    'threeBlindMice.ts' 파일이 기존 lyric 속성을 사용하여 발생하던 TypeScript 오류(TS2353)를 해결했습니다.
    
    가사 다국어 지원을 위해 해당 파일의 가사를 새로운 lyricKey 시스템으로 마이그레이션하고, i18n.js에 번역 리소스를 추가했습니다.
    
    이 변경으로 모든 곡의 가사가 일관된 다국어 시스템을 사용하게 되었습니다.

--- src/songs/threeBlindMice.ts
+++ src/songs/threeBlindMice.ts
@@ -3,39 +3,39 @@ import { SongNote } from './types';
 export const threeBlindMice: SongNote[][] = [
   // 1소절: Three blind mice, Three blind mice
   [
-    { note: 'E4', duration: 'q', lyric: 'Three' }, { note: 'D4', duration: 'q', lyric: 'blind' },
-    { note: 'C4', duration: 'h', lyric: 'mice' }, { note: 'E4', duration: 'q', lyric: 'Three' },
-    { note: 'D4', duration: 'q', lyric: 'blind' }, { note: 'C4', duration: 'h', lyric: 'mice' },
+    { note: 'E4', duration: 'q', lyricKey: 'lyrics.threeBlindMice.0.0' }, { note: 'D4', duration: 'q', lyricKey: 'lyrics.threeBlindMice.0.1' },
+    { note: 'C4', duration: 'h', lyricKey: 'lyrics.threeBlindMice.0.2' }, { note: 'E4', duration: 'q', lyricKey: 'lyrics.threeBlindMice.0.3' },
+    { note: 'D4', duration: 'q', lyricKey: 'lyrics.threeBlindMice.0.4' }, { note: 'C4', duration: 'h', lyricKey: 'lyrics.threeBlindMice.0.5' },
   ],
   // 2소절: See how they run, See how they run
   [
-    { note: 'G4', duration: 'q', lyric: 'See' }, { note: 'F4', duration: 'q', lyric: 'how' },
-    { note: 'E4', duration: 'h', lyric: 'they run' }, { note: 'G4', duration: 'q', lyric: 'See' },
-    { note: 'F4', duration: 'q', lyric: 'how' }, { note: 'E4', duration: 'h', lyric: 'they run' },
+    { note: 'G4', duration: 'q', lyricKey: 'lyrics.threeBlindMice.1.0' }, { note: 'F4', duration: 'q', lyricKey: 'lyrics.threeBlindMice.1.1' },
+    { note: 'E4', duration: 'h', lyricKey: 'lyrics.threeBlindMice.1.2' }, { note: 'G4', duration: 'q', lyricKey: 'lyrics.threeBlindMice.1.3' },
+    { note: 'F4', duration: 'q', lyricKey: 'lyrics.threeBlindMice.1.4' }, { note: 'E4', duration: 'h', lyricKey: 'lyrics.threeBlindMice.1.5' },
   ],
   // 3소절: They all ran after the farmer's wife
   [
-    { note: 'G4', duration: 'q', lyric: 'They' }, { note: 'A4', duration: 'q', lyric: 'all' },
-    { note: 'G4', duration: 'q', lyric: 'ran' }, { note: 'F4', duration: 'q', lyric: 'af-' },
-    { note: 'E4', duration: 'q', lyric: 'ter' }, { note: 'D4', duration: 'q', lyric: 'the' },
-    { note: 'C4', duration: 'h', lyric: 'wife,' },
+    { note: 'G4', duration: 'q', lyricKey: 'lyrics.threeBlindMice.2.0' }, { note: 'A4', duration: 'q', lyricKey: 'lyrics.threeBlindMice.2.1' },
+    { note: 'G4', duration: 'q', lyricKey: 'lyrics.threeBlindMice.2.2' }, { note: 'F4', duration: 'q', lyricKey: 'lyrics.threeBlindMice.2.3' },
+    { note: 'E4', duration: 'q', lyricKey: 'lyrics.threeBlindMice.2.4' }, { note: 'D4', duration: 'q', lyricKey: 'lyrics.threeBlindMice.2.5' },
+    { note: 'C4', duration: 'h', lyricKey: 'lyrics.threeBlindMice.2.6' },
   ],
   // 4소절: Who cut off their tails with a carving knife
   [
-    { note: 'G4', duration: 'q', lyric: 'Who' }, { note: 'A4', duration: 'q', lyric: 'cut' },
-    { note: 'G4', duration: 'q', lyric: 'off' }, { note: 'F4', duration: 'q', lyric: 'their' },
-    { note: 'E4', duration: 'q', lyric: 'tails' }, { note: 'D4', duration: 'q', lyric: 'with a' },
-    { note: 'C4', duration: 'h', lyric: 'knife,' },
+    { note: 'G4', duration: 'q', lyricKey: 'lyrics.threeBlindMice.3.0' }, { note: 'A4', duration: 'q', lyricKey: 'lyrics.threeBlindMice.3.1' },
+    { note: 'G4', duration: 'q', lyricKey: 'lyrics.threeBlindMice.3.2' }, { note: 'F4', duration: 'q', lyricKey: 'lyrics.threeBlindMice.3.3' },
+    { note: 'E4', duration: 'q', lyricKey: 'lyrics.threeBlindMice.3.4' }, { note: 'D4', duration: 'q', lyricKey: 'lyrics.threeBlindMice.3.5' },
+    { note: 'C4', duration: 'h', lyricKey: 'lyrics.threeBlindMice.3.6' },
   ],
   // 5소절: Did you ever see such a sight in your life
   [
-    { note: 'C4', duration: 'q', lyric: 'Did' }, { note: 'D4', duration: 'q', lyric: 'you' },
-    { note: 'E4', duration: 'h', lyric: 'e-ver' }, { note: 'C4', duration: 'q', lyric: 'see' },
-    { note: 'D4', duration: 'q', lyric: 'such a' }, { note: 'E4', duration: 'h', lyric: 'sight,' },
+    { note: 'C4', duration: 'q', lyricKey: 'lyrics.threeBlindMice.4.0' }, { note: 'D4', duration: 'q', lyricKey: 'lyrics.threeBlindMice.4.1' },
+    { note: 'E4', duration: 'h', lyricKey: 'lyrics.threeBlindMice.4.2' }, { note: 'C4', duration: 'q', lyricKey: 'lyrics.threeBlindMice.4.3' },
+    { note: 'D4', duration: 'q', lyricKey: 'lyrics.threeBlindMice.4.4' }, { note: 'E4', duration: 'h', lyricKey: 'lyrics.threeBlindMice.4.5' },
   ],
   // 6소절: As three blind mice
   [
-    { note: 'D4', duration: 'q', lyric: 'As' }, { note: 'D4', duration: 'q', lyric: 'three' },
-    { note: 'C4', duration: 'h', lyric: 'blind mice.' },
+    { note: 'D4', duration: 'q', lyricKey: 'lyrics.threeBlindMice.5.0' }, { note: 'D4', duration: 'q', lyricKey: 'lyrics.threeBlindMice.5.1' },
+    { note: 'C4', duration: 'h', lyricKey: 'lyrics.threeBlindMice.5.2' },
   ],
 ];
